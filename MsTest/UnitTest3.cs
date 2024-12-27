d84ec2f90ec3202a0d08279c87b84f89d2d629f0
Nikita Krenov <nikitabaram@gmail.com>
2024-12-27 19:22:58 +0100

    ### Summary of Most Important Changes The code changes introduce a new unit test class `UnitTest3` to test the comparison operators of a newly added `Task3` class. The `Task3` class includes a `Balance` property and several operator overloads, as well as overrides for `Equals` and `GetHashCode` methods to ensure proper comparison and hashing based on the `Balance` property.
    ### List of Changes
    1. **Introduction of `UnitTest3` Class**:
       - A new unit test class `UnitTest3` is added in the `MsTest` namespace.
       - This class contains two test methods: `TestMethod1` and `TestMethod2`.
       - Reference: `UnitTest3` class in `MsTest` namespace.
    
    2. **Addition of `Task3` Class**:
       - A new class `Task3` is added in the `overloading_operators_Indexers_and_properties` namespace.
       - This class includes a `Balance` property.
       - Reference: `Task3` class in `overloading_operators_Indexers_and_properties` namespace.
    
    3. **Operator Overloads in `Task3` Class**:
       - The `Task3` class includes operator overloads for `+`, `-`, `==`, `!=`, `<`, and `>`.
       - Reference: Operator overloads in `Task3` class.
    
    4. **Overrides of `Equals` and `GetHashCode` Methods**:
       - The `Task3` class overrides the `Equals` and `GetHashCode` methods to ensure proper comparison and hashing based on the `Balance` property.
       - Reference: `Equals` and `GetHashCode` methods in `Task3` class.
    
    5. **Implementation of `TestMethod1` in `UnitTest3`**:
       - `TestMethod1` creates two instances of `Task3`, sets their balances, and asserts that the first instance is less than the second.
       - Reference: `TestMethod1` in `UnitTest3`.
    
    6. **Implementation of `TestMethod2` in `UnitTest3`**:
       - `TestMethod2` creates two instances of `Task3`, sets their balances, and asserts that the first instance is not less than the second.
       - Reference: `TestMethod2` in `UnitTest3`.

---
+++ MsTest/UnitTest3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTest
+{
+    [TestClass]
+    internal class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Task3 card1 = new Task3();
+            Task3 card2 = new Task3();
+            card1.Balance = 1000;
+            card2.Balance = 2000;
+            Assert.IsTrue(card1 < card2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Task3 card1 = new Task3();
+            Task3 card2 = new Task3();
+            card1.Balance = 3000;
+            card2.Balance = 4000;
+            Assert.IsFalse(card1 < card2);
+        }
+    }
+}
---
+++ overloading_operators_Indexers_and_properties/Task3.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overloading_operators_Indexers_and_properties
+{
+    internal class Task3
+    {
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public static Task3 operator +(Task3 card, decimal amount)
+        {
+            card.Balance += amount;
+            return card;
+        }
+
+        public static Task3 operator -(Task3 card, decimal amount)
+        {
+            card.Balance -= amount;
+            return card;
+        }
+
+        public static bool operator ==(Task3 card1, Task3 card2)
+        {
+            return card1.Balance == card2.Balance;
+        }
+
+        public static bool operator !=(Task3 card1, Task3 card2)
+        {
+            return card1.Balance != card2.Balance;
+        }
+
+        public static bool operator <(Task3 card1, Task3 card2)
+        {
+            return card1.Balance < card2.Balance;
+        }
+
+        public static bool operator >(Task3 card1, Task3 card2)
+        {
+            return card1.Balance > card2.Balance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Task3 otherCard = (Task3)obj;
+            return Balance == otherCard.Balance;
+        }
+
+        public override int GetHashCode()
+        {
+            return Balance.GetHashCode();
+        }
+    }
+}
